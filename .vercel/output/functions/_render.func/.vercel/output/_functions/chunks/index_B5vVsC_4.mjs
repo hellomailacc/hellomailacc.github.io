4aa791b303a351d3f8d25ddde69bb13ac2c24114
mahmut-a <xmahmut@proton.me>
2024-10-02 14:04:43 +0300

    VS Vercel 5

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_B5vVsC_4.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./index_BUsMb7nD.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
